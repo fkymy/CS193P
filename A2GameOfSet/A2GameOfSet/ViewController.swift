428f71a2dccc4f4ba549ed6e2f46fdebb6fa271d
Yuske Fukuyama <yuskefuku@gmail.com>
2018-03-11 00:57:58 +0900

    A1 outlets

--- A2GameOfSet/A2GameOfSet/ViewController.swift
+++ A2GameOfSet/A2GameOfSet/ViewController.swift
@@ -12,14 +12,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            button.layer.borderWidth = 2.0
+            button.layer.borderColor = UIColor.blue.cgColor
+            button.layer.cornerRadius = 8.0
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func tapCardButton(_ sender: UIButton) {
+        if let tappedIndex = cardButtons.index(of: sender) {
+            print(tappedIndex)
+        }
+    }
+    
+    @IBAction func onNewGame(_ sender: UIButton) {
+    }
+    
+    @IBAction func onDrawCards(_ sender: UIButton) {
+    }
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBOutlet weak var setsFoundLabel: UILabel!
+    @IBOutlet weak var cardsInDeckLabel: UILabel!
+    
 }
+
 
