4f8f8c54ebdf777ec38042f2657ac4bf582e2c37
Yuske Fukuyama <yuskefuku@gmail.com>
2018-03-19 15:14:01 +0900

    L6 custom view

--- PlayingCard/PlayingCard/PlayingCard.swift
+++ PlayingCard/PlayingCard/PlayingCard.swift
@@ -17,7 +17,9 @@ struct PlayingCard: CustomStringConvertible {
     var rank: Rank
     
     enum Suit: String, CustomStringConvertible {
-        var description: String
+        var description: String {
+            return self.rawValue
+        }
         case spades = "♠️"
         case hearts = "♥️"
         case clubs = "♣️"
@@ -27,7 +29,13 @@ struct PlayingCard: CustomStringConvertible {
     }
     
     enum Rank: CustomStringConvertible {
-        var description: String
+        var description: String {
+            switch self {
+            case .ace: return "A"
+            case .numeric(let pips): return String(pips)
+            case .face(let kind): return kind
+            }
+        }
 
         case ace
         case face(String) // bad design
