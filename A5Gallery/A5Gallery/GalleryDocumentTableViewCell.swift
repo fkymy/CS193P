119097ae6044b946e79a6fbda6204730e0b15aff
Yuske Fukuyama <yuskefuku@gmail.com>
2018-05-29 20:53:15 +0900

    eupho

---
+++ A5Gallery/A5Gallery/GalleryDocumentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GalleryDocumentTableViewCell.swift
+//  A5Gallery
+//
+//  Created by Yuske Fukuyama on 2018/05/27.
+//  Copyright © 2018 Yuske Fukuyama. All rights reserved.
+//
+
+import UIKit
+
+class GalleryDocumentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
